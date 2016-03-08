3dd3f0865266cc4dbcb8bf0e6d00e6f6f48b3e73
PIYU <piyusha_zanjad@csu.fullerton.edu>
2016-03-07 22:59:00 -0800

    updated homework3.js

--- Assignment-03/homework3.js
+++ Assignment-03/homework3.js
@@ -1,3 +1,7 @@
+"use strict";
+/*globals _*/
+/*jshint browser:true*/
+/*jshint unused:false*/
 var exercise1 = function (numbers) {
 
     var sum = 0;
@@ -7,72 +11,6 @@ var exercise1 = function (numbers) {
     return sum / numbers.length;
 
 };
-
-
-var click1 = function (name, input) {
-    var array = document.getElementById(input).value;
-    var result = [];
-    result = array.split(',');
-
-    var len = result.length;
-    for (var i = 0; i < len; i++) {
-        result[i] = parseInt(result[i], 10);
-    }
-
-    if (name == "exercise1") {
-
-        document.getElementById("answer").innerHTML = exercise1(result).toString();
-    }
-    else if (name == "exercise2") {
-        document.getElementById("answer1").innerHTML = exercise2(result).toString();
-    }
-    else if (name == "exercise3") {
-        document.getElementById("answer2").innerHTML = exercise3(result).toString();
-    }
-    else if (name == "exercise4") {
-        document.getElementById("answer3").innerHTML = exercise4(result).toString();
-    }
-    else if (name == "max") {
-        document.getElementById("answer8").innerHTML = max(result).toString();
-    }
-
-    else if (name == "atleast1") {
-        document.getElementById("answer9").innerHTML = atleast1(result).toString();
-    }
-    else if (name == "all_even") {
-        document.getElementById("answer10").innerHTML = all_even(result).toString();
-    }
-
-};
-
-var click2 = function (name, input1, input2) {
-
-    var array = document.getElementById(input1).value;
-    var array1 = document.getElementById(input2).value;
-    var result = [];
-    result = array.split(',');
-
-    if (name == "arrayContains") {
-        document.getElementById("answer4").innerHTML = arrayContains(result, array1).toString();
-    }
-    else if (name == "arrayContainsTwo") {
-        document.getElementById("answer5").innerHTML = arrayContainsTwo(result, array1).toString();
-    }
-};
-
-var click3 = function (name, input1, input2, input3) {
-    var array = document.getElementById(input1).value;
-    var array1 = document.getElementById(input2).value;
-    var array2 = document.getElementById(input3).value;
-
-    var result = [];
-    result = array.split(',');
-
-    if (name == "arrayContainsNTimes") {
-        document.getElementById("answer6").innerHTML = arrayContainsNTimes(result, array1, array2).toString();
-    }
-};
-
 
 var exercise2 = function (num) {
     var large = num[0];
@@ -122,7 +60,7 @@ var arrayContains = function (str1, str2) {
 var arrayContainsTwo = function (str1, str2) {
     var count = 0;
     for (var i = 0; i < str1.length; i++) {
-        if (str1[i] == str2) {
+        if (str1[i] === str2) {
             count++;
         }
     }
@@ -137,11 +75,11 @@ var arrayContainsTwo = function (str1, str2) {
 var arrayContainsNTimes = function (str1, str2, num) {
     var count = 0;
     for (var i = 0; i < str1.length; i++) {
-        if (str1[i] == str2) {
+        if (str1[i] === str2) {
             count++;
         }
     }
-    if (count == num) {
+    if (count <= num) {
         return true;
     }
     else {
@@ -163,4 +101,68 @@ var all_even = function (number) {
     var all = _.every(number, function (num) { return num % 2 === 0; });
     return all;
 };
- 
+
+
+var click1 = function (name, input) {
+    var array = document.getElementById(input).value;
+    var result = [];
+    result = array.split(",");
+
+    var len = result.length;
+    for (var i = 0; i < len; i++) {
+        result[i] = parseInt(result[i], 10);
+    }
+
+    if (name === "exercise1") {
+
+        document.getElementById("answer").innerHTML = exercise1(result).toString();
+    }
+    else if (name === "exercise2") {
+        document.getElementById("answer1").innerHTML = exercise2(result).toString();
+    }
+    else if (name === "exercise3") {
+        document.getElementById("answer2").innerHTML = exercise3(result).toString();
+    }
+    else if (name === "exercise4") {
+        document.getElementById("answer3").innerHTML = exercise4(result).toString();
+    }
+    else if (name === "max") {
+        document.getElementById("answer8").innerHTML = max(result).toString();
+    }
+
+    else if (name === "atleast1") {
+        document.getElementById("answer9").innerHTML = atleast1(result).toString();
+    }
+    else if (name === "all_even") {
+        document.getElementById("answer10").innerHTML = all_even(result).toString();
+    }
+
+};
+
+var click2 = function (name, input1, input2) {
+
+    var array = document.getElementById(input1).value;
+    var array1 = document.getElementById(input2).value;
+    var result = [];
+    result = array.split(",");
+
+    if (name === "arrayContains") {
+        document.getElementById("answer4").innerHTML = arrayContains(result, array1).toString();
+    }
+    else if (name === "arrayContainsTwo") {
+        document.getElementById("answer5").innerHTML = arrayContainsTwo(result, array1).toString();
+    }
+};
+
+var click3 = function (name, input1, input2, input3) {
+    var array = document.getElementById(input1).value;
+    var array1 = document.getElementById(input2).value;
+    var array2 = document.getElementById(input3).value;
+
+    var result = [];
+    result = array.split(",");
+
+    if (name === "arrayContainsNTimes") {
+        document.getElementById("answer6").innerHTML = arrayContainsNTimes(result, array1, array2).toString();
+    }
+};
